97ac2eb1d382cfe104e0a8f4831c205a4043fcc7
Soumyadeep Chatterjee <soumyadeepchatterjee@Soumyadeeps-MacBook-Air.local>
2026-03-06 02:50:28 +0530

    pathvariable & postconstruct

--- RestCRUDAPIs/demo/src/main/java/com/example/demo/rest/StudentRestController.java
+++ RestCRUDAPIs/demo/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.example.demo.rest;
 
 import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,25 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    //end point for "/api/students"
-    @GetMapping("/students")
-    public List<Student> students(){
-        List<Student> list = new ArrayList<>();
+    private List<Student> list;
+
+    @PostConstruct
+    public void loadData(){
+        list = new ArrayList<>();
 
         list.add(new Student("Soumyadeep", "Chatterjee"));
         list.add(new Student("Papia", "Chatterjee"));
         list.add(new Student("Chanchal", "Chatterjee"));
+    }
 
+    //end point for "/api/students"
+    @GetMapping("/students")
+    public List<Student> students(){
         return list;
+    }
+
+    @GetMapping("/students/{studentid}")
+    public Student getStudent(@PathVariable int studentid){
+        return list.get(studentid);
     }
 }
